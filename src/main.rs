c7a7bea27434d4fcb56a93fcebb9481d13f23b0d
P. Todd Decker <ptdecker@mac.com>
2023-12-25 11:01:53 -0600

    Refactor
    Moved thread pool into its own module simplifying lib down to just a module list.
    Extracted Server into its own struct and simplified main.rs
    Added basic comments

--- src/main.rs
+++ src/main.rs
@@ -1,14 +1,5 @@
-use ptodd::{server::handle_connection, ThreadPool};
-use std::net::TcpListener;
+use ptodd::server::Server;
 
 fn main() {
-    let addr = "127.0.0.1:6502";
-    let listener = TcpListener::bind(addr).unwrap();
-    let pool = ThreadPool::build(4).unwrap();
-    println!("Listening for connections on {}.", addr);
-    for stream in listener.incoming() {
-        let stream = stream.unwrap();
-        pool.execute(|| handle_connection(stream));
-    }
-    println!("Shutting down.");
+    Server::new("127.0.0.1:6502").run();
 }
--- src/server.rs
+++ src/server.rs
@@ -1,10 +1,45 @@
+use crate::pool::ThreadPool;
 use std::{
     fs,
     io::{prelude::*, BufReader},
-    net::TcpStream,
+    net::{TcpListener, TcpStream},
     thread,
     time::Duration,
 };
+
+/// Default contants for the server.
+const DEFAULT_POOL_SIZE: usize = 4;
+
+/// A server, which listens for incoming connections and handles them.
+#[derive(Debug)]
+pub struct Server {
+    /// The address to bind the server to.
+    addr: String,
+    /// The listener, which listens for incoming connections.
+    listener: TcpListener,
+    /// The thread pool, which manages our worker threads.
+    pool: ThreadPool,
+}
+
+impl Server {
+    pub fn new(addr: impl Into<String>) -> Server {
+        let addr = addr.into();
+        Server {
+            addr: addr.clone(),
+            listener: TcpListener::bind(&addr).unwrap(),
+            pool: ThreadPool::build(DEFAULT_POOL_SIZE).unwrap(),
+        }
+    }
+
+    pub fn run(&self) {
+        println!("Listening for connections on {}.", &self.addr);
+        for stream in self.listener.incoming() {
+            let stream = stream.unwrap();
+            self.pool.execute(|| handle_connection(stream));
+        }
+        println!("Shutting down.");
+    }
+}
 
 pub fn handle_connection(mut stream: TcpStream) {
     let buf_reader = BufReader::new(&mut stream);
